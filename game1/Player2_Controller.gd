68028423da68a1b1dbebc32eb745c9d7135e4d2c
Breno <gallobreno@gmail.com>
2025-09-09 09:28:22 -0300

    trava player na tela

--- game1/Player2_Controller.gd
+++ game1/Player2_Controller.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("down_player2"):
-		self.position.y += 100 * delta
+		self.position.y += 200 * delta
 	if Input.is_action_pressed("up_player2"):
-		self.position.y -= 100 * delta
+		self.position.y -= 200 * delta
+	if (self.position.y - 60 <= 0):
+		self.position.y = 60
+	if (self.position.y + 60 >= 648):
+		self.position.y = 588
