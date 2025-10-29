4ba3239c9c66805ecc7fb9b8c98fcd4ef33510a8
Breno <gallobreno@gmail.com>
2025-10-29 15:13:20 -0300

    a

--- novo-projeto-de-jogo/scenes/main.gd
+++ novo-projeto-de-jogo/scenes/main.gd
@@ -2,40 +2,104 @@ extends Node
 
 @export var snake_scene : PackedScene
 
-#game variables
 var score : int
 var game_started : bool = false
+var high_score : int = 0 
+var high_score_file_path = "user://highscore.txt" 
 
-#grid variables
 var cells : int = 20
 var cell_size : int = 50
 
-#food variables
 var food_pos : Vector2
 var regen_food : bool = true
+var current_food_type : String = "apple" 
 
-#snake variables
-var old_data : Array
-var snake_data : Array
-var snake : Array
+@onready var apple = $Food
+@onready var grape = $Grape
+@onready var pause_menu = $PauseMenu 
+@onready var result_label = $GameOverMenu/ResultLabel 
 
-#movement variables
+@onready var power_up = $PowerUp 		
+@onready var power_down = $PowerDown 	
+var power_timer : Timer 
+var current_power_type : String = "" 
+var power_pos : Vector2
+var power_active : bool = false 
+
+var power_up_effect_timer : Timer   
+var power_down_effect_timer : Timer 
+var score_multiplier_active : bool = false
+var original_move_speed : float 
+
+var power_despawn_timer : Timer 
+
+const POWER_EFFECT_DURATION = 10.0 
+const POWER_DESPAWN_DURATION = 5.0 
+const POWER_DOWN_SLOW_FACTOR = 2.5 
+
+var old_data : Array = []
+var snake_data : Array = []
+var snake : Array = []
+
 var start_pos = Vector2(9, 9)
 var up = Vector2(0, -1)
 var down = Vector2(0, 1)
 var left = Vector2(-1, 0)
 var right = Vector2(1, 0)
 var move_direction : Vector2
-var can_move: bool
+var can_move : bool
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	process_mode = PROCESS_MODE_ALWAYS
+	
+	power_timer = Timer.new()
+	power_timer.wait_time = 15.0
+	power_timer.one_shot = false   
+	power_timer.timeout.connect(_on_power_timer_timeout) 
+	add_child(power_timer) 
+	
+	power_up_effect_timer = Timer.new()
+	power_up_effect_timer.wait_time = POWER_EFFECT_DURATION
+	power_up_effect_timer.one_shot = true 
+	power_up_effect_timer.timeout.connect(_on_power_up_effect_timeout) 
+	add_child(power_up_effect_timer)
+	
+	power_down_effect_timer = Timer.new()
+	power_down_effect_timer.wait_time = POWER_EFFECT_DURATION
+	power_down_effect_timer.one_shot = true 
+	power_down_effect_timer.timeout.connect(_on_power_down_effect_timeout) 
+	add_child(power_down_effect_timer)
+
+	power_despawn_timer = Timer.new()
+	power_despawn_timer.wait_time = POWER_DESPAWN_DURATION
+	power_despawn_timer.one_shot = true 
+	power_despawn_timer.timeout.connect(_on_power_despawn_timeout) 
+	add_child(power_despawn_timer)
+	
+	original_move_speed = $MoveTimer.wait_time
+	
+	load_highscore() 
 	new_game()
 	
 func new_game():
 	get_tree().paused = false
 	get_tree().call_group("segments", "queue_free")
+	
+	power_timer.stop() 
+	power_up_effect_timer.stop()
+	power_down_effect_timer.stop()
+	power_despawn_timer.stop() 
+	$MoveTimer.wait_time = original_move_speed 
+	
 	$GameOverMenu.hide()
+	pause_menu.hide() 
+	
+	power_up.hide()
+	power_down.hide()
+	power_active = false
+	current_power_type = ""
+	score_multiplier_active = false 
+	
 	score = 0
 	$Hud.get_node("ScoreLabel").text = "SCORE: " + str(score)
 	move_direction = up
@@ -47,7 +111,6 @@ func generate_snake():
 	old_data.clear()
 	snake_data.clear()
 	snake.clear()
-	#starting with the start_pos, create tail segments vertically down
 	for i in range(3):
 		add_segment(start_pos + Vector2(0, i))
 		
@@ -58,13 +121,21 @@ func add_segment(pos):
 	add_child(SnakeSegment)
 	snake.append(SnakeSegment)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel") and game_started:
+		if get_tree().is_paused():
+			if pause_menu.visible:
+				unpause_game()
+		else:
+			pause_game()
+			
+	if get_tree().is_paused():
+		return 
+			
 	move_snake()
 	
 func move_snake():
 	if can_move:
-		#update movement from keypresses
 		if Input.is_action_just_pressed("move_down") and move_direction != up:
 			move_direction = down
 			can_move = false
@@ -89,22 +160,20 @@ func move_snake():
 func start_game():
 	game_started = true
 	$MoveTimer.start()
-
+	power_timer.start() 
 
 func _on_move_timer_timeout():
-	#allow snake movement
 	can_move = true
-	#use the snake's previous position to move the segments
 	old_data = [] + snake_data
 	snake_data[0] += move_direction
 	for i in range(len(snake_data)):
-		#move all the segments along by one
 		if i > 0:
 			snake_data[i] = old_data[i - 1]
 		snake[i].position = (snake_data[i] * cell_size) + Vector2(0, cell_size)
 	check_out_of_bounds()
 	check_self_eaten()
 	check_food_eaten()
+	check_power_eaten() 
 	
 func check_out_of_bounds():
 	if snake_data[0].x < 0 or snake_data[0].x > cells - 1 or snake_data[0].y < 0 or snake_data[0].y > cells - 1:
@@ -116,9 +185,18 @@ func check_self_eaten():
 			end_game()
 			
 func check_food_eaten():
-	#if snake eats the food, add a segment and move the food
 	if snake_data[0] == food_pos:
-		score += 1
+		if score_multiplier_active:
+			if current_food_type == "apple":
+				score += 2 
+			elif current_food_type == "grape":
+				score += 10 
+		else:
+			if current_food_type == "apple":
+				score += 1
+			elif current_food_type == "grape":
+				score += 5
+		
 		$Hud.get_node("ScoreLabel").text = "SCORE: " + str(score)
 		add_segment(old_data[-1])
 		move_food()
@@ -126,19 +204,160 @@ func check_food_eaten():
 func move_food():
 	while regen_food:
 		regen_food = false
+		var chance = randf() 
+		if chance <= 0.8:
+			current_food_type = "apple"
+		else:
+			current_food_type = "grape"
+
 		food_pos = Vector2(randi_range(0, cells - 1), randi_range(0, cells - 1))
 		for i in snake_data:
 			if food_pos == i:
 				regen_food = true
-	$Food.position = (food_pos * cell_size)+ Vector2(0, cell_size)
+	
+	if current_food_type == "apple":
+		apple.show()
+		grape.hide()
+		apple.position = (food_pos * cell_size) + Vector2(0, cell_size)
+	else:
+		grape.show()
+		apple.hide()
+		grape.position = (food_pos * cell_size) + Vector2(0, cell_size)
+
 	regen_food = true
 
 func end_game():
-	$GameOverMenu.show()
 	$MoveTimer.stop()
+	power_timer.stop() 
+	power_up_effect_timer.stop() 
+	power_down_effect_timer.stop()
+	power_despawn_timer.stop() 
 	game_started = false
+	
+	if score > high_score:
+		high_score = score
+		save_highscore()
+		
+	result_label.text = "SCORE: " + str(score) + "\nHIGH SCORE: " + str(high_score)
+	
+	$GameOverMenu.show()
 	get_tree().paused = true
 
 
 func _on_game_over_menu_restart():
 	new_game()
+
+func pause_game():
+	get_tree().paused = true
+	$MoveTimer.stop() 
+	power_timer.stop() 
+	
+	power_up_effect_timer.paused = true
+	power_down_effect_timer.paused = true
+	power_despawn_timer.paused = true 
+	
+	pause_menu.show()
+
+func unpause_game():
+	get_tree().paused = false
+	pause_menu.hide()
+	$MoveTimer.start() 
+	power_timer.start() 
+	
+	power_up_effect_timer.paused = false
+	power_down_effect_timer.paused = false
+	power_despawn_timer.paused = false 
+
+func load_highscore():
+	if FileAccess.file_exists(high_score_file_path):
+		var file = FileAccess.open(high_score_file_path, FileAccess.READ)
+		if file:
+			var content = file.get_as_text()
+			if content.is_valid_int():
+				high_score = int(content)
+			file.close()
+	else:
+		high_score = 0
+
+func save_highscore():
+	var file = FileAccess.open(high_score_file_path, FileAccess.WRITE)
+	if file:
+		file.store_string(str(high_score))
+		file.close()
+
+func _on_power_timer_timeout():
+	if power_active:
+		return
+
+	if randf() > 0.5:
+		current_power_type = "up"
+	else:
+		current_power_type = "down"
+
+	var regen_power_pos = true
+	while regen_power_pos:
+		regen_power_pos = false
+		power_pos = Vector2(randi_range(0, cells - 1), randi_range(0, cells - 1))
+		
+		for i in snake_data:
+			if power_pos == i:
+				regen_power_pos = true
+				break 
+		
+		if regen_power_pos:
+			continue 
+			
+		if power_pos == food_pos:
+			regen_power_pos = true
+			continue
+	
+	if current_power_type == "up":
+		power_up.show()
+		power_up.position = (power_pos * cell_size) + Vector2(0, cell_size)
+	else: 
+		power_down.show()
+		power_down.position = (power_pos * cell_size) + Vector2(0, cell_size)
+
+	power_active = true
+	power_despawn_timer.start() 
+
+func check_power_eaten():
+	if not power_active:
+		return
+
+	if snake_data[0] == power_pos:
+		power_despawn_timer.stop() 
+		
+		power_up.hide()
+		power_down.hide()
+		
+		if current_power_type == "up":
+			score_multiplier_active = true
+			power_up_effect_timer.start() 
+			
+		elif current_power_type == "down":
+			if $MoveTimer.wait_time == original_move_speed:
+				$MoveTimer.wait_time = original_move_speed * POWER_DOWN_SLOW_FACTOR
+			
+			power_down_effect_timer.start() 
+		
+		power_active = false
+		current_power_type = ""
+
+func _on_power_up_effect_timeout():
+	score_multiplier_active = false
+
+func _on_power_down_effect_timeout():
+	$MoveTimer.wait_time = original_move_speed
+
+func _on_power_despawn_timeout():
+	if not power_active:
+		return
+		
+	power_up.hide()
+	power_down.hide()
+	
+	power_active = false
+	current_power_type = ""
+	
+	power_timer.start()
