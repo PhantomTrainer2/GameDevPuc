473483a6e65340a00e763066414b08a6e63681ee
Breno <gallobreno@gmail.com>
2025-12-01 08:58:47 -0300

    merge

--- TelePlataformerLv1/Scripts/menu.gd
+++ TelePlataformerLv1/Scripts/menu.gd
@@ -10,3 +10,6 @@ var current_code_index = 0
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
